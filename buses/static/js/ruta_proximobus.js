1a29c2fe6e84fd39844c0e69eb75aa67fcd75aad
Fabián Abarca <ensinergia@gmail.com>
2025-07-21 15:06:29 -0300

    Paso a paso

--- buses/static/js/ruta_proximobus.js
+++ buses/static/js/ruta_proximobus.js
@@ -1,133 +1,168 @@
-ruta_app.component('proximobus', {
-    props: ['route_short_name'],
-    template: `
-<div class="container proxbus-container">
+const { ref, onMounted, onBeforeUnmount } = Vue;
 
-<div class="row proxbus-row proxbus-emph-row">
-<div class="col">
-                {{ route_short_name }} <i class="fas fa-arrow-circle-right color-ruta"></i> San&nbsp;José
-</div>
-<div class="col">
-                  San José <i class="fas fa-arrow-circle-right color-ruta"></i> {{ route_short_name }}
-</div>
-</div>
+ruta_app.component("proximobus", {
+  props: ["route_short_name"],
+  setup() {
+    // Reactive data
+    const desde_sanjose = ref(["-", "-", "-"]); // Horas
+    const hacia_sanjose = ref(["-", "-", "-"]);
+    const desde_sanjose_ramal = ref([]); // Ramales
+    const hacia_sanjose_ramal = ref([]);
 
-<!-- Primera fila -->
-<div class="row proxbus-row font-weight-bold">
-    <div class="col proxbus-emph-col" v-if="hacia_sanjose[0]">
-        <span class="align-middle">{{ hacia_sanjose[0] }}</span>&nbsp;
-        <span v-if="is_badge_visible(hacia_sanjose_ramal[0])"
-            :class="['badge', 'custom-badge',
-            filter_badge(hacia_sanjose_ramal[0])
-            ]">{{ hacia_sanjose_ramal[0] }}</span>
-    </div>
-    <div class="col" v-else>No hay más buses hoy</div>
+    // Store interval reference for cleanup
+    let intervalId = null;
 
-    <div class="col proxbus-emph-col" v-if="desde_sanjose[0]">
-        <span class="align-middle">{{ desde_sanjose[0] }}</span>&nbsp;
-        <span v-if="is_badge_visible(desde_sanjose_ramal[0])"
-            :class="['badge', 'custom-badge',
-            filter_badge(desde_sanjose_ramal[0])
-            ]">{{ desde_sanjose_ramal[0] }}</span>
-    </div>
-    <div class="col" v-else>No hay más buses hoy</div>
-</div>
+    // Methods
+    const is_badge_visible = (ramal) => {
+      // Es visible si ramal tiene un valor y no es "SG"
+      if (ramal && ramal != "SG") return true;
+      return false;
+    };
 
-<!-- Segunda fila -->
-<div class="row proxbus-row font-weight-bold">
-<div class="col">
-        <span v-if="hacia_sanjose[1]" class="align-middle">{{ hacia_sanjose[1] }}</span>&nbsp;
-        <span v-if="is_badge_visible(hacia_sanjose_ramal[1])"
-            :class="['badge', 'custom-badge',
-            filter_badge(hacia_sanjose_ramal[1])
-            ]">{{ hacia_sanjose_ramal[1] }}</span>
-    </div>
-    <div class="col">
-        <span v-if="desde_sanjose[1]" class="align-middle">{{ desde_sanjose[1] }}</span>&nbsp;
-        <span v-if="is_badge_visible(desde_sanjose_ramal[1])"
-            :class="['badge', 'custom-badge',
-            filter_badge(desde_sanjose_ramal[1])
-            ]">{{ desde_sanjose_ramal[1] }}</span>
-    </div>
-</div>
+    const badgeClassMap = {
+      SG: "invisible",
+      AC: "badge-secondary",
+      SL: "fondo-color-sanluis",
+      TU: "fondo-color-turrujal",
+      JO: "badge-secondary",
+    };
 
-<!-- Tercera fila -->
-<div class="row proxbus-row font-weight-bold">
-    <div class="col">
-        <span v-if="hacia_sanjose[2]" class="align-middle">{{ hacia_sanjose[2] }}</span>&nbsp;
-        <span v-if="is_badge_visible(hacia_sanjose_ramal[2])"
-            :class="['badge', 'custom-badge',
-            filter_badge(hacia_sanjose_ramal[2])
-            ]">{{ hacia_sanjose_ramal[2] }}</span>
-    </div>
-    <div class="col">
-        <span v-if="desde_sanjose[2]" class="align-middle">{{ desde_sanjose[2] }}</span>&nbsp;
-        <span v-if="is_badge_visible(desde_sanjose_ramal[2])"
-            :class="['badge', 'custom-badge',
-            filter_badge(desde_sanjose_ramal[2])
-            ]">{{ desde_sanjose_ramal[2] }}</span>
-    </div>
-</div>
-</div>
+    const filter_badge = (ramal) => {
+      return badgeClassMap[ramal] || "";
+    };
+
+    // Function to update the next bus times
+    const updateProximoBus = () => {
+      desde_sanjose.value = [];
+      desde_sanjose_ramal.value = [];
+      let time = new Date();
+      let time_to_minutes = time.getMinutes() + time.getHours() * 60;
+
+      horario_desde_sanjose
+        .filter((value) => value[0] > time_to_minutes) // Mayores a la hora
+        .slice(0, 3) // Los siguientes 3
+        .forEach((element, index) => {
+          time.setHours(element[1]);
+          time.setMinutes(element[2]);
+          desde_sanjose.value[index] = time.toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          });
+          desde_sanjose_ramal.value[index] = element[3]; // Ramal
+        });
+
+      hacia_sanjose.value = [];
+      hacia_sanjose_ramal.value = [];
+      horario_hacia_sanjose
+        .filter((value) => value[0] > time_to_minutes) // Mayores a la hora
+        .slice(0, 3) // Los siguientes 3
+        .forEach((element, index) => {
+          time.setHours(element[1]);
+          time.setMinutes(element[2]);
+          hacia_sanjose.value[index] = time.toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          });
+          hacia_sanjose_ramal.value[index] = element[3]; // Ramal
+        });
+    };
+
+    // Lifecycle hooks
+    onMounted(() => {
+      updateProximoBus();
+      intervalId = setInterval(() => {
+        updateProximoBus();
+      }, 60000); // Cada minuto refresca
+    });
+
+    onBeforeUnmount(() => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    });
+
+    // Return all reactive data and methods that template needs
+    return {
+      desde_sanjose,
+      hacia_sanjose,
+      desde_sanjose_ramal,
+      hacia_sanjose_ramal,
+      is_badge_visible,
+      filter_badge,
+    };
+  },
+  template: `
+<table class="table table-light">
+
+<thead class="lead">
+    <th>
+        {{ route_short_name }}
+    </th>
+    <th>
+        San&nbsp;José
+    </th>
+</thead>
+
+<tbody>
+    <!-- Primera fila -->
+    <tr class="lead">
+        <td v-if="hacia_sanjose[0]">
+            <span class="align-middle">{{ hacia_sanjose[0] }}</span>&nbsp;
+            <span v-if="is_badge_visible(hacia_sanjose_ramal[0])"
+                :class="['badge', 'custom-badge',
+                filter_badge(hacia_sanjose_ramal[0])
+                ]">{{ hacia_sanjose_ramal[0] }}</span>
+        </td>
+        <td v-else>No hay más buses hoy</td>
+
+        <td v-if="desde_sanjose[0]">
+            <span class="align-middle">{{ desde_sanjose[0] }}</span>&nbsp;
+            <span v-if="is_badge_visible(desde_sanjose_ramal[0])"
+                :class="['badge', 'custom-badge',
+                filter_badge(desde_sanjose_ramal[0])
+                ]">{{ desde_sanjose_ramal[0] }}</span>
+        </td>
+        <td v-else>No hay más buses hoy</td>
+    </tr>
+
+    <!-- Segunda fila -->
+    <tr>
+        <td>
+            <span v-if="hacia_sanjose[1]" class="align-middle">{{ hacia_sanjose[1] }}</span>&nbsp;
+            <span v-if="is_badge_visible(hacia_sanjose_ramal[1])"
+                :class="['badge', 'custom-badge',
+                filter_badge(hacia_sanjose_ramal[1])
+                ]">{{ hacia_sanjose_ramal[1] }}</span>
+        </td>
+        <td>
+            <span v-if="desde_sanjose[1]" class="align-middle">{{ desde_sanjose[1] }}</span>&nbsp;
+            <span v-if="is_badge_visible(desde_sanjose_ramal[1])"
+                :class="['badge', 'custom-badge',
+                filter_badge(desde_sanjose_ramal[1])
+                ]">{{ desde_sanjose_ramal[1] }}</span>
+        </td>
+    </tr>
+
+    <!-- Tercera fila -->
+    <tr>
+        <td>
+            <span v-if="hacia_sanjose[2]" class="align-middle">{{ hacia_sanjose[2] }}</span>&nbsp;
+            <span v-if="is_badge_visible(hacia_sanjose_ramal[2])"
+                :class="['badge', 'custom-badge',
+                filter_badge(hacia_sanjose_ramal[2])
+                ]">{{ hacia_sanjose_ramal[2] }}</span>
+        </td>
+        <td>
+            <span v-if="desde_sanjose[2]" class="align-middle">{{ desde_sanjose[2] }}</span>&nbsp;
+            <span v-if="is_badge_visible(desde_sanjose_ramal[2])"
+                :class="['badge', 'custom-badge',
+                filter_badge(desde_sanjose_ramal[2])
+                ]">{{ desde_sanjose_ramal[2] }}</span>
+        </td>
+    </tr>
+</tbody>
+</table>
 `,
-    data: function () {
-        return {
-            desde_sanjose: ['-','-','-'], // Horas
-            hacia_sanjose: ['-','-','-'],
-
-            desde_sanjose_ramal: [], // Ramales
-            hacia_sanjose_ramal: [],
-        };
-    },
-    methods: {
-        is_badge_visible(ramal){
-            // Si tiene length y no es SG
-            if ( ramal && ( ramal != 'SG' ))  return true;
-            return false;
-        },
-        filter_badge (ramal){
-            if (ramal == 'SG') return 'invisible';
-            if (ramal == 'AC') return 'badge-secondary';
-            if (ramal == 'SL') return 'fondo-color-sanluis';
-            if (ramal == 'TU') return 'fondo-color-turrujal';
-            if (ramal == 'JO') return 'badge-secondary';
-            return '';
-        },
-        updateProximoBus: function(){
-            // Lógica de proximobus
-            this.desde_sanjose = [];
-            var time = new Date();
-            var time_to_minutes = time.getMinutes() + time.getHours()*60;
-            horario_desde_sanjose.filter(
-                value => value[0] > time_to_minutes ) // Mayores a la hora
-                .slice(0,3) // Los siguientes 3
-                .forEach((element, index) => {
-                    time.setHours(element[1]);
-                    time.setMinutes(element[2]);
-                    this.desde_sanjose[index] = time.toLocaleString(
-                        'en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-                    this.desde_sanjose_ramal[index] = element[3]; // Ramal
-                });
-
-            this.hacia_sanjose = [];
-            horario_hacia_sanjose.filter(
-                value => value[0] > time_to_minutes ) // Mayores a la hora
-                .slice(0,3) // Los siguientes 3
-                .forEach((element, index) => {
-                    time.setHours(element[1]);
-                    time.setMinutes(element[2]);
-                    this.hacia_sanjose[index] = time.toLocaleString(
-                        'en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-                    this.hacia_sanjose_ramal[index] = element[3]; // Ramal
-                });
-        },
-    },
-    mounted: function () {
-        setInterval(() => {
-            this.updateProximoBus();
-        }, 1000); // Cada segundo refresca
-    },
-    created: function (){
-        this.updateProximoBus();
-    },
 });
